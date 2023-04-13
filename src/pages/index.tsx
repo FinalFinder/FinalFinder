94f07163f7bcdd7fca41585f0eabc9f4ac7b5015
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-04-13 17:30:15 -0400

    Run prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>FinalFinder</title>
-        <meta name="description" content="A web app and Slack integration to help Hack Clubbers study for finals" />
+        <meta
+          name="description"
+          content="A web app and Slack integration to help Hack Clubbers study for finals"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
