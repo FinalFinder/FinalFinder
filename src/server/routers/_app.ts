92c8a809c37595f0d52c2cc192b42b4a00d37e9b
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-04-19 22:18:06 -0400

    Add all exams tRPC procedure

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,6 +1,10 @@
-import { z } from "zod";
-import { procedure, router } from "../trpc";
+import { protectedProcedure, router } from "../trpc";
+import prisma from "@/db/prisma";
 
-export const appRouter = router({});
+export const appRouter = router({
+  allExams: protectedProcedure.query(async () => {
+    return await prisma.exam.findMany();
+  }),
+});
 
 export type AppRouter = typeof appRouter;
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,22 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 
-const t = initTRPC.create();
+import type { Context } from "./context";
+
+const t = initTRPC.context<Context>().create();
+
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.session?.user?.email) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    });
+  }
+  return next({
+    ctx: {
+      session: ctx.session,
+    },
+  });
+});
 
 export const router = t.router;
-export const procedure = t.procedure;
+export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuthed);
