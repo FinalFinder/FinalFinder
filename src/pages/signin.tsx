76bab5f697cd091e9a76e3e3ea3dbd8b9bbd90dc
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-04-22 14:45:01 -0400

    Skip sign in process if already signed in

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
-import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { signIn, useSession } from "next-auth/react";
 
 let signingIn = false;
 
 export default function SignIn() {
+  const { status } = useSession();
+  const router = useRouter();
+
   useEffect(() => {
-    if (signingIn) return;
+    if (signingIn || status === "loading") return;
+    if (status === "authenticated") {
+      router.push("/home");
+      return;
+    }
     signingIn = true;
     signIn("slack", { callbackUrl: "/home" }).then(() => {
       signingIn = false;
     });
-  }, []);
+  }, [status, router]);
 
   return <p className="text-center text-2xl">Signing in...</p>;
 }
