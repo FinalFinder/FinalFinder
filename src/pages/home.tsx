2f5bd45dedbb6935636875814bc923bc39f678c2
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-04-22 14:58:16 -0400

    Create basic home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -16,7 +16,22 @@ export default function Home() {
 
   if (userExams.isSuccess && userExams.data?.length === 0) {
     router.push("/edit");
+    return null;
   }
 
-  return null;
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <h1 className="text-center text-2xl font-bold">
+        Welcome back, {session?.user?.name}!
+      </h1>
+
+      <div className="my-2 w-5/6 rounded-md bg-gray-2 p-2">
+        <h2 className="text-center text-xl">Upcoming Exams</h2>
+      </div>
+
+      <div className="my-2 w-5/6 rounded-md bg-gray-2 p-2">
+        <h2 className="text-center text-xl">Study Sessions</h2>
+      </div>
+    </div>
+  );
 }
