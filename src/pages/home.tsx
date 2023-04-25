423b980d74a53dbfb4f9d2056af3a697ce527e97
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-04-25 17:19:10 -0400

    Refactor error messages into a component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import Exam from "@/components/Exam";
 import Button from "@/components/Button";
 import StudySession from "@/components/Session";
+import ErrorComponent from "@/components/Error";
 
 import { trpc } from "@/utils/trpc";
 
@@ -21,40 +22,18 @@ export default function Home() {
 
   if (userExams.error)
     return (
-      <p className="m-2 rounded-md bg-red-600 p-2 text-2xl">
-        Uh oh, there was an error fetching your exams. Please try refreshing. If
-        the issue persists,{" "}
-        <a
-          href="https://github.com/polypixeldev/FinalFinder/issues/new"
-          target="_blank"
-          className="text-white underline"
-        >
-          file an issue on our GitHub
-        </a>{" "}
-        and include the following error message: <br />
-        <code className="mt-2 block w-full rounded-md bg-gray-2 p-1 text-white">
-          {userExams.error.message ?? "amog"}
-        </code>
-      </p>
+      <ErrorComponent
+        error="fetching your exams"
+        message={userExams.error.message}
+      />
     );
 
   if (userSessions.error)
     return (
-      <p className="m-2 rounded-md bg-red-600 p-2 text-2xl">
-        Uh oh, there was an error fetching your study sessions. Please try
-        refreshing. If the issue persists,{" "}
-        <a
-          href="https://github.com/polypixeldev/FinalFinder/issues/new"
-          target="_blank"
-          className="text-white underline"
-        >
-          file an issue on our GitHub
-        </a>{" "}
-        and include the following error message: <br />
-        <code className="mt-2 block w-full rounded-md bg-gray-2 p-1 text-white">
-          {userSessions.error.message ?? "amog"}
-        </code>
-      </p>
+      <ErrorComponent
+        error="fetching your study sessions"
+        message={userSessions.error.message}
+      />
     );
 
   if (userExams.isSuccess && userExams.data?.length === 0) {
